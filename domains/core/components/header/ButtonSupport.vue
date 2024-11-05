7ad0d2719a288030a7fd98b60d172434c2614d2f
Ivan Alvarez <i.alvarez@tiendasdaka.com>
2024-11-04 21:20:48 -0400

    feat(button): support button

---
+++ domains/core/components/header/ButtonSupport.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { SfButton } from "@storefront-ui/vue";
+
+const NuxtLink = resolveComponent("NuxtLink");
+</script>
+
+<template>
+  <!-- <ClientOnly> -->
+  <SfButton
+    class="text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 mr-1 -ml-0.5 rounded-md"
+    variant="tertiary"
+    to="/login"
+    :tag="NuxtLink"
+    square
+  >
+    <template #prefix>
+      <Icon name="ion:call" size="22px" />
+    </template>
+  </SfButton>
+  <!-- 
+    <template #fallback>
+      <SfButton
+        class="group relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 rounded-md"
+        variant="tertiary"
+        square
+      >
+        <template #prefix>
+          <Icon name="ion:person-outline" size="22px" />
+        </template>
+      </SfButton>
+    </template>
+  </ClientOnly> -->
+</template>
